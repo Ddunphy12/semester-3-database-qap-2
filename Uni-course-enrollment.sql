71d6a782627d7640886760f7cd040fdcb0717d62
Zackery Strickland <Zachary.Strickland@keyin.com>
2025-02-06 17:05:23 -0330

    Created students table

--- Uni-course-enrollment.sql
+++ Uni-course-enrollment.sql
@@ -1,1 +1,9 @@
 -- Problem 1: University Course Enrollment
+
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50) UNIQUE,
+    school_enroll_date DATE
+);
