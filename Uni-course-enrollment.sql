906203ef7f1ade2d72bbbab6f9120c3c57c0d1ea
Zackery Strickland <Zachary.Strickland@keyin.com>
2025-02-07 18:49:31 -0330

    Created professor, corses and enrollment table

--- Uni-course-enrollment.sql
+++ Uni-course-enrollment.sql
@@ -7,3 +7,24 @@ CREATE TABLE students (
     email VARCHAR(50) UNIQUE,
     school_enroll_date DATE
 );
+
+CREATE TABLE professors (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department VARCHAR(100)
+);
+
+CREATE TABLE courses (
+    id SERIAL PRIMARY KEY,
+    course_name VARCHAR(100),
+    course_description TEXT,
+    professor_id INT REFERENCES professors(id)
+);
+
+CREATE TABLE enrollments (
+    student_id INT REFERENCES students(id),
+    course_id INT REFERENCES courses(id),
+    enrollment_date DATE,
+    PRIMARY KEY (student_id, course_id)
+);
